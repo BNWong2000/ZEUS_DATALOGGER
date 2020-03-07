2abb39cfae84626accdb4fe5a78a6f62572f6709
Branden Wong <branden.wong1@ucalgary.ca>
2020-03-07 12:15:42 -0700

    Added some GPIO functions.

--- Main.py
+++ Main.py
@@ -1,10 +1,37 @@
 import time
 
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO")
+
+outputInit = False;
+inputInit = False;
+
 def printStuff():
     print("hehehe")
+
+def setupInputs():
+    inputList = [1, 2] #All the channels for input. 
+    GPIO.setup(inputList, GPIO.IN)
+    inputInit = True
+
+def setupOutputs():
+    outputList = [3, 4] #All the channels for output.
+    GPIO.setup(outputList, GPIO.OUT)
+    outputInit = True
+    
+
+def initializePins():
+    GPIO.setmode(GPIO.BOARD) #Sets the pin numbering scheme. don't change this.
+    setupInputs()
+    setupOutputs()
     
 def main():
+    initializePins()
+    
     printStuff()
+
     
 if __name__ == "__main__":
     main()
