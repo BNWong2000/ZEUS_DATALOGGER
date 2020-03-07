de31f55378cddc173f2ae6748100173059402781
Branden Wong <branden.wong1@ucalgary.ca>
2020-03-07 15:59:02 -0700

    Changed file open/closing.

--- Main.py
+++ Main.py
@@ -6,38 +6,43 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO")
 
-
 counter = 0
+myFile = open("logging.txt", "w")
 
 def printStuff():
     print("hehehe")
 
 def initializePins():
-    GPIO.setmode(GPIO.BOARD) #Sets the pin numbering scheme. don't change this.
+    GPIO.setmode(GPIO.BOARD)  #Sets the pin numbering scheme. don't change this.
     print("pins initialized")
 
 def logTime():
-    time.sleep(1) 
-    global counter
-    counter = counter + 1
-    writeCounterToFile()
-    
+    try:
+    	global myFile
+	time.sleep(1)
+        global counter
+   	counter = counter + 1
+    	writeCounterToFile()
+    except KeyboardInterrupt: #When the user does cntrl+c, a keyboard interrupt is triggered, thereby killing the program. 
+	myFile.close() #before the program ends, make sure to close the file. 
+	print("Ending Program")
+
 def writeCounterToFile():
     global counter
     print(counter)
-    myFile = open("logging.txt", "a")
+    #myFile = open("logging.txt", "a")
     myFile.write("Time: " + str(counter) + "s\n" )
-    myFile.close();
+    #myFile.close();
 
 def main():
     initializePins()
     printStuff()
-    myFile = open("logging.txt", "w")
-    myFile.close() 
+    #myFile = open("logging.txt", "w")
+    #myFile.close()
     while True:
 	logTime()
-    print("Ending Program")
+    #print("Ending Program")
 
-    
+
 if __name__ == "__main__":
     main()
