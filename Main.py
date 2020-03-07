0aa0ec48cfe1077e1491882c3e829a57321d7ccb
Branden Wong <branden.wong1@ucalgary.ca>
2020-03-07 13:46:23 -0700

    Added a time logging and file writing feature.

--- Main.py
+++ Main.py
@@ -1,36 +1,42 @@
 import time
+import string
 
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO")
 
-outputInit = False;
-inputInit = False;
+
+counter = 0
 
 def printStuff():
     print("hehehe")
 
-def setupInputs():
-    inputList = [1, 2] #All the channels for input. 
-    GPIO.setup(inputList, GPIO.IN)
-    inputInit = True
-
-def setupOutputs():
-    outputList = [3, 4] #All the channels for output.
-    GPIO.setup(outputList, GPIO.OUT)
-    outputInit = True
-    
-
 def initializePins():
     GPIO.setmode(GPIO.BOARD) #Sets the pin numbering scheme. don't change this.
-    setupInputs()
-    setupOutputs()
+    print("pins initialized")
+
+def logTime():
+    time.sleep(1) 
+    global counter
+    counter = counter + 1
+    writeCounterToFile()
     
+def writeCounterToFile():
+    global counter
+    print(counter)
+    myFile = open("logging.txt", "a")
+    myFile.write("Time: " + str(counter) + "s\n" )
+    myFile.close();
+
 def main():
     initializePins()
-    
     printStuff()
+    myFile = open("logging.txt", "w")
+    myFile.close() 
+    while True:
+	logTime()
+    print("Ending Program")
 
     
 if __name__ == "__main__":
